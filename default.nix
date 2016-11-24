8720e829761c12ab6cd5fb5b4108a30c74578078
Alberto Valverde <alberto@toscat.net>
2016-11-24 17:14:39 +0100

    allow extraPackages to be configured to be available to the plugin's ghc

--- default.nix
+++ default.nix
@@ -1,46 +1,53 @@
 {
-  gdal-plugin-hs-dso = {stdenv, ghc, gdal}: stdenv.mkDerivation rec {
-    version = "1.0";
-    shortname = "gdal-plugin-hs-dso";
-    name = "${shortname}-${version}";
+  gdal-plugin-hs-dso =
+    { stdenv , ghcWithPackages , gdal, extraPackages? ps: [] }:
+    let ghc = ghcWithPackages
+          (pkgs: [
+            pkgs.gdal-plugin-hs
+            pkgs.bindings-gdal
+            ] ++ extraPackages pkgs);
+    in stdenv.mkDerivation rec {
+      version = "1.0";
+      shortname = "gdal-plugin-hs-dso";
+      name = "${shortname}-${version}";
 
-    src = ./dso;
+      src = ./dso;
 
-    buildInputs = [
-      gdal
-      ghc
-    ];
+      buildInputs = [
+        gdal
+        ghc
+      ];
 
 
-    patchPhase = "make clean";
+      patchPhase = "make clean";
 
-    installPhase = ''
-      mkdir -p $out/lib
-      install -m 0755 gdal_HS.so $out/lib/
-      '';
+      installPhase = ''
+        mkdir -p $out/lib
+        install -m 0755 gdal_HS.so $out/lib/
+        '';
 
-    doCheck = true;
+      doCheck = true;
 
-    checkPhase = ''
-      export GDAL_DRIVER_PATH="$(pwd)"
-      gdalinfo --formats | grep Haskell
-      '';
+      checkPhase = ''
+        export GDAL_DRIVER_PATH="$(pwd)"
+        gdalinfo --formats | grep Haskell
+        '';
 
-    shellHook = ''
-      export GDAL_DRIVER_PATH="$(pwd)/dso:$GDAL_DRIVER_PATH"
+      shellHook = ''
+        export GDAL_DRIVER_PATH="$(pwd)/dso:$GDAL_DRIVER_PATH"
 
-      # Para que el compilador encuentre los paquetes y funcione ghc-mod
-      export NIX_GHC="${ghc}/bin/ghc"
-      export NIX_GHCPKG="${ghc}/bin/ghc-pkg"
-      export NIX_GHC_DOCDIR="${ghc}/share/doc/ghc/html"
-      export NIX_GHC_LIBDIR="${ghc}/lib/ghc-${ghc.version}"
-      '';
+        # Para que el compilador encuentre los paquetes y funcione ghc-mod
+        export NIX_GHC="${ghc}/bin/ghc"
+        export NIX_GHCPKG="${ghc}/bin/ghc-pkg"
+        export NIX_GHC_DOCDIR="${ghc}/share/doc/ghc/html"
+        export NIX_GHC_LIBDIR="${ghc}/lib/ghc-${ghc.version}"
+        '';
 
-    meta = {
-      description = "GDAL which embeds GHC";
-      homepage = https://github.com/meteogrid/gdal-plugin-hs;
-      license = stdenv.lib.licenses.bsd3;
+      meta = {
+        description = "GDAL which embeds GHC";
+        homepage = https://github.com/meteogrid/gdal-plugin-hs;
+        license = stdenv.lib.licenses.bsd3;
+      };
     };
-  };
   gdal-plugin-hs = import ./cabal.nix;
 }
